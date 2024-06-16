0bade8cc126eacb1a40ba89ff1dc6a234e2444f7
soden46 <syarifsoden22@gmail.com>
2024-06-16 23:09:02 +0700

    Update

---
+++ resources/views/siswa/pendaftaran/index.blade.php
@@ -0,0 +1,50 @@
+@extends('dashboard', [
+    'title' => 'Data Pendaftaran',
+    'pageTitle' => 'Data Pendaftaran',
+])
+@section('content')
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+    <div class="d-flex justify-content-between mb-3">
+        <div>
+            <form action="{{ route('siswa.pendaftaran') }}" method="GET" class="d-flex">
+                <input type="text" name="cari" class="form-control" placeholder="Cari Data" value="{{ request('cari') }}">
+                <button type="submit" class="btn btn-md btn-primary ml-2">Search</button>
+            </form>
+        </div>
+    </div>
+    <table class="table table-bordered">
+        <tr class="font-12">
+            <th style="width: 150px">Id Pendaftaran</th>
+            <th style="width: 150px">Nama Siswa</th>
+            <th style="width: 150px">Nama Rekrutmen</th>
+            <th style="width: 150px">Tanggal</th>
+            <th style="width: 150px">Status</th>
+            {{-- <th style="width: 100px">Aksi</th> --}}
+        </tr>
+        @foreach ($pendaftaran as $data)
+            <tr>
+                <td style="width: 150px">{{ $data->id_pendaftaran }}</td>
+                <td style="width: 150px">{{ $data->siswa->users->nama ?? '' }}</td>
+                <td style="width: 150px">{{ $data->rekrutmen->nama_lowongan ?? '' }}</td>
+                <td style="width: 150px">{{ $data->tanggal }}</td>
+                <td style="width: 150px">{{ $data->status }}</td>
+                {{-- <td>
+                    <div class="btn-group" style="width:135px">
+                        <form action="{{ route('siswa.pendaftaran.destroy', $data->id_pendaftaran) }}" method="Post">
+                            <a class="btn btn-primary"
+                                href="{{ route('siswa.pendaftaran.edit', $data->id_pendaftaran) }}">Edit</a>
+                            @csrf
+                        </form>
+                    </div>
+                </td> --}}
+            </tr>
+        @endforeach
+    </table>
+    <div class="row text-center">
+        {!! $pendaftaran->links() !!}
+    </div>
+@endsection
